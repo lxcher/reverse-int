416586b363188383d54aca128bc4ea88482b462b
lxcher <aleksejcerenkevic@gmail.com>
2022-06-21 20:13:57 +0300

    fix: add inner to function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let arr = Array.from(String(n))
+  if (arr[0] === '-') {
+    let chars = arr.slice(1, arr.length).reverse()
+    return chars.join('')
+    } else {
+        return arr.reverse().join('')
+    }
 }
